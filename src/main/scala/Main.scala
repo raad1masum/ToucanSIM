095b51983cb2de091e0917bd1e208da0daee823c
raad1masum <piraadwan@gmail.com>
2020-12-19 08:16:28 -0800

    more rocket variables

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,14 +5,14 @@ import ToucanSIM.Physics._
 import ToucanSIM.Simulation._
 
 object Main extends App {
-	printf("Enter Mass of Rocket: ")
-	val mass: Double = scala.io.StdIn.readDouble()
-	printf("Enter Thrust of Rocket: ")
-	val thrust: Double = scala.io.StdIn.readDouble()
-	printf("Enter Thrust Time of Rocket: ")
-	val thrustTime: Double = scala.io.StdIn.readDouble()
+	// printf("Enter Mass of Rocket: ")
+	// val mass: Double = scala.io.StdIn.readDouble()
+	// printf("Enter Thrust of Rocket: ")
+	// val thrust: Double = scala.io.StdIn.readDouble()
+	// printf("Enter Thrust Time of Rocket: ")
+	// val thrustTime: Double = scala.io.StdIn.readDouble()
 
-  val rocket = new Rocket(mass, thrust, thrustTime)
+  val rocket = new Rocket(98286580, 450767420, 1451496, 7607000)
 
   val sim = new Simulation(rocket)
 }
--- src/main/scala/Rocket.scala
+++ src/main/scala/Rocket.scala
@@ -1,11 +1,15 @@
 package ToucanSIM
 
 object Rocket {
-  class Rocket(val mass: Double, val thrust: Double, val thrustTime: Double) {
-  	def report() {
-  		println("Mass: " + mass)
-  		println("Thrust: " + thrust)
-  		println("Thrust Time: " + thrustTime)
-  	}
+  class Rocket(val mass: Double, val fuelMass: Double, val consumptionRate: Double, val thrust: Double) {
+  	var increment: Double = 0.01
+  	var time: Double = 0.0
+  	var height: Double = 0.0
+  	val gravity: Double = -9.8
+
+    def report() {
+      println("Mass: " + mass)
+      println("Thrust: " + thrust)
+    }
   }
 }
