01448b759ddde6b50ac886955e610561e1eb1599
raad1masum <piraadwan@gmail.com>
2020-12-19 08:23:11 -0800

    sim docs

--- src/main/scala/Simulation.scala
+++ src/main/scala/Simulation.scala
@@ -2,6 +2,11 @@ package ToucanSIM
 
 import ToucanSIM.Rocket._
 
+/** Simulate rocket launch
+ *
+ *  @constructor Simulates rocket launch.
+ *  @param rocket Rocket profile.
+ */
 object Simulation {
 	class Simulation(val rocket: Rocket) {
 		rocket.report()
