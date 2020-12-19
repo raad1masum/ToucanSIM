eb6b0bef171454cd5e7d7bd210e8d4910022f1e2
raad1masum <piraadwan@gmail.com>
2020-12-19 09:34:53 -0800

    clean up

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,8 +8,5 @@ object Main extends App {
   val rocket = new Rocket(98286580, 450767420, 1451496, 7607000)
   val otherRocket = new Rocket(1000, 100, 60, 10)
 
-  println(rocket.currentMass())
-  println(otherRocket.currentMass())
-
   val sim = new Simulation(rocket)
 }
