45853a94fff181b5889d9e157ef94ca50a80a5cc
raad1masum <piraadwan@gmail.com>
2020-12-20 12:34:36 -0800

    use falcon 9 stats

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,11 +5,10 @@ import ToucanSIM.Physics._
 import ToucanSIM.Simulation._
 
 object Main extends App {
-  val rocket = new Rocket(98286580, 450767420, 1451496, 7607000)
-  val otherRocket = new Rocket(1000, 100, 60, 10)
+	// SpaceX Falcon 9
+  val rocket = new Rocket(22200000, 433100000, 1090931.99, 7607000)
 
   val sim = new Simulation(rocket)
-  val otherSim = new Simulation(otherRocket)
   sim.run
 
   val phys = new Physics(sim)
