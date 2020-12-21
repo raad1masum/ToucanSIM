b4dc5edc157f00d74e29018e330866f39d56da22
raad1masum <piraadwan@gmail.com>
2020-12-21 09:57:57 -0800

    choose rocket to run

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,15 +5,35 @@ import ToucanSIM.Physics._
 import ToucanSIM.Simulation._
 
 object Main extends App {
+	println(Console.BLUE + "ToucanSIM")
+
 	// SpaceX Falcon 9
   val Falcon9 = new Rocket(22200000, 433100000, 1090931.99, 7607000)
 
 	// NASA Saturn V
   val SaturnV = new Rocket(130000000, 615962205.49, 18143695, 33361662.211881433)
 
-  val sim = new Simulation(Falcon9)
-  sim.run
+  def run(rocket: Rocket) {
+  	val sim = new Simulation(rocket)
+  	val phys = new Physics(sim)
 
-  val phys = new Physics(sim)
-  phys.plot
+  	sim.run
+  	phys.plot
+  }
+
+  println("Select a rocket to test:")
+  println("1. Falcon9")
+  println("2. SaturnV")
+  println("3. Custom")
+
+  val selectedRocket: Int = scala.io.StdIn.readLine().toInt
+
+  val rocket: Rocket = selectedRocket match {
+    case 1  => Falcon9
+    case 2  => SaturnV
+    case 3  => Falcon9
+    case _  => Falcon9
+  }
+
+  run(rocket)
 }
