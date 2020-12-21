2ed7abeab2f727be1edfa18ab05d2c2f9aeb9ad4
raad1masum <piraadwan@gmail.com>
2020-12-21 09:15:10 -0800

    add saturn v

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,9 +6,12 @@ import ToucanSIM.Simulation._
 
 object Main extends App {
 	// SpaceX Falcon 9
-  val rocket = new Rocket(22200000, 433100000, 1090931.99, 7607000)
+  val Falcon9 = new Rocket(22200000, 433100000, 1090931.99, 7607000)
 
-  val sim = new Simulation(rocket)
+	// NASA Saturn V
+  val SaturnV = new Rocket(130000000, 615962205.49, 18143695, 33361662.211881433)
+
+  val sim = new Simulation(Falcon9)
   sim.run
 
   val phys = new Physics(sim)
