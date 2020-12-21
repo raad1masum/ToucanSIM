2752e2c020a7c98bf1d30323f93a715dce183f25
raad1masum <piraadwan@gmail.com>
2020-12-21 11:31:11 -0800

    custom rocket

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,6 +13,29 @@ object Main extends App {
 	// NASA Saturn V
   val SaturnV = new Rocket("NASA Saturn V", 130000000, 615962205.49, 18143695, 33361662.211881433)
 
+  def custom: Rocket = {
+  	println("ToucanSIM Rocket Configurator")
+
+  	print("Enter Rocket Name: ")
+  	val name: String = scala.io.StdIn.readLine()
+
+  	print("Enter Rocket Mass (grams): ")
+  	val mass: Double = scala.io.StdIn.readLine().toDouble
+
+  	print("Enter Fuel Mass (grams): ")
+  	val fuelMass: Double = scala.io.StdIn.readLine().toDouble
+
+  	print("Enter Fuel Consumption Rate (grams/second): ")
+  	val consumptionRate: Double = scala.io.StdIn.readLine().toDouble
+
+  	print("Enter Engine Thrust (newtons): ")
+  	val thrust: Double = scala.io.StdIn.readLine().toDouble
+
+  	val customRocket = new Rocket(name, mass, fuelMass, consumptionRate, thrust)
+
+  	customRocket
+  }
+
   def run(rocket: Rocket) {
   	val sim = new Simulation(rocket)
   	val phys = new Physics(sim)
@@ -31,8 +54,8 @@ object Main extends App {
   val rocket = selectedRocket match {
     case 1  => Falcon9: Rocket
     case 2  => SaturnV: Rocket
-    case 3  => Falcon9: Rocket
-    case _  => Falcon9: Rocket
+    case 3  => custom
+    case _  => custom
   }
 
   val name: String = rocket.name
